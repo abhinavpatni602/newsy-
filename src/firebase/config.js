173d7c936b8c6e3da55e9c6a1e3ed18e8bbeaf14
abhinavpatni602 <64361930+abhinavpatni602@users.noreply.github.com>
2020-08-27 00:27:30 +0530

    Version 1.1

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDSQMqbjoKK5pb-_Rvg7z4j1MW_yW2A9QU",
+    authDomain: "newsy-83d54.firebaseapp.com",
+    databaseURL: "https://newsy-83d54.firebaseio.com",
+    projectId: "newsy-83d54",
+    storageBucket: "newsy-83d54.appspot.com",
+    messagingSenderId: "404113653707",
+    appId: "1:404113653707:web:57ee2c8a3151f11ed3c04e"
+  };
+
+export default firebaseConfig;
